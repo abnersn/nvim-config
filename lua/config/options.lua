98675bc59b47057ed0078d8fa3db982d08af9666
Abner Nascimento <abner.nascimento@hp.com>
2024-10-08 16:28:59 -0300

    Update config.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,33 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local is_msys = string.find(vim.env.shell, "msys") ~= nil
+
+local opt = vim.opt
+
+-- WSL Clipboard support
+if is_msys then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+  vim.env.shell='/usr/bin/zsh'
+  vim.o.shellcmdflag='-c'
+  vim.o.shellxquote='('
+end
+
+-- Don't care about these
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+
 vim.opt.relativenumber = false
